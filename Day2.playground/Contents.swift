7e93d140aaf99311e1b3d486efbfedb343bbed0a
parkdaxun <parkde0207@gmail.com>
2023-03-06 16:43:59 +0900

    Day2 : 반복문

--- Day2.playground/Contents.swift
+++ Day2.playground/Contents.swift
@@ -1,6 +1,10 @@
 import UIKit
 
-var myArrary : [Int] = [0,1,2,3,4,5,6,7,8,9,10]
+var myArray : [Int] = [0,1,2,3,4,5,6,7,8,9,10]
 
 // 콜렉션 : 데이터 모아둔 곳
 // 배열, 셋, 딕셔너리, 튜플
+
+for item in myArray where item%2==0 {
+    print("짝수는 : \(item)")
+}
